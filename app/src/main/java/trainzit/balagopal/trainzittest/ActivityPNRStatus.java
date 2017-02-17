67f595d7e0f1d78f6084af2f51692bfcb2c78ab2
Balagopal CK <Balagopal@macs-MacBook-Pro.local>
2017-02-17 12:55:15 +0530

    Initial commit

---
+++ app/src/main/java/trainzit/balagopal/trainzittest/ActivityPNRStatus.java
@@ -0,0 +1,13 @@
+package trainzit.balagopal.trainzittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityPNRStatus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pnrstatus);
+    }
+}
